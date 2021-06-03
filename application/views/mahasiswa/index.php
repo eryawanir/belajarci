e2d6c2dc10341f0a5b1d6d6a95383bc005f34d9e
Muhammad Eryawan <mochyawn@gmail.com>
2021-06-03 19:40:32 +0700

    add view mahasiswa

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group  mt-3">
+                <li class="list-group-item">An item</li>
+            </ul>
+        </div>
+    </div>
+</div>
