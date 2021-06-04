7b3eef725cf38718b3f6e7b933665d82da90a5f4
Muhammad Eryawan <mochyawn@gmail.com>
2021-06-04 16:24:28 +0700

    buat view model mahasiwa

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -3,7 +3,9 @@
         <div class="col-md-6">
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group  mt-3">
-                <li class="list-group-item">An item</li>
+                <?php foreach ($mahasiswa as $data) : ?>
+                    <li class="list-group-item"><?= $data['nama']; ?></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
